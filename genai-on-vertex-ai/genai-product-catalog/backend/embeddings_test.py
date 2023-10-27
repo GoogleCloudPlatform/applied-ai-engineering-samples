01f1f4fdd251507ebae7c4e99ba5b9aed149b83a
Vijay Reddy <reddyv@google.com>
2023-10-27 16:49:39 +0000

    import test settings from config

--- genai-on-vertex-ai/genai-product-catalog/backend/embeddings_test.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/embeddings_test.py
@@ -1,23 +1,23 @@
 """Vertex Embedding API Integration Test.
 
 Ensures we can call the Vertex Embedding API and embeddings are returned in 
-the expected format. As this is a cloud API this test will need to run from
-a GCP authenticated environment
+the expected format. This integration test assumes:
+-The appropriate constants have been set in config.py
+-The test is run from an environment that has permission to call the API
 
 https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-multimodal-embeddings
 """
 import unittest
+import config
 import embeddings
-
-PROJECT_ID = 'solutions-2023-mar-107'
 
 class EmbeddingsTest(unittest.TestCase):
 
   def test_embeddings_api(self):
     res = embeddings.embed(
-        PROJECT_ID,
-        "IZOD Women's Light Gray Thigh Length Pull On Golf Shorts",
-        'gs://genai-product-catalog/toy_images/shorts.jpg',
+        config.PROJECT,
+        'This is a test description',
+        config.GCS_TEST_IMAGE,
     )
     self.assertEqual(len(res.text_embedding), 1408)
     self.assertEqual(len(res.image_embedding), 1408)
