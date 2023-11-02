8dfe65c595fbbd746118f1f0e65dd69b8e8bc597
Vijay Reddy <reddyv@google.com>
2023-11-02 16:35:51 +0000

    cloud run works with auth locally and in cloud

--- genai-on-vertex-ai/genai-product-catalog/backend/api.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/api.py
@@ -1,17 +1,17 @@
-"""
-A sample Hello World server.
-"""
+"""Exposes REST API for product cataloging functionality."""
 import os
 
 from flask import Flask
 from flask_restful import Resource, Api
+
+import category, config
 
 app = Flask(__name__)
 api = Api(app)
 
 class Category(Resource):
     def get(self):
-        return {'hello': 'category'}
+        return category.join_categories([config.TEST_PRODUCT_ID])
 
 api.add_resource(Category, '/')
 if __name__ == '__main__':
