c78654b7f93d9a431a0ce5ad26d3ec01955d2229
Vijay Reddy <reddyv@google.com>
2023-11-07 18:37:04 +0000

    add auth for testing deployed API

--- genai-on-vertex-ai/genai-product-catalog/backend/api_test.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/api_test.py
@@ -2,27 +2,48 @@
 
 Make sure to update the ENDPOINT variable as appropriate before running.
 """
-import logging
+import logging; logging.basicConfig(level=logging.INFO)
 import requests
 import unittest
 import api
 import config
 
-ENDPOINT = 'http://localhost:8080/v1/'
-#ENDPOINT = 'https://product-catalog-u2uyzke6ea-uc.a.run.app/v1/'
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
 
+ENDPOINT = 'http://localhost:8080/v1/'
+ENDPOINT = 'https://vijay-sandbox-335018.uc.r.appspot.com/v1/'
+
+open_id_connect_token = id_token.fetch_id_token(Request(), 'client_id')
+headers = {
+    "Authorization": f"Bearer {open_id_connect_token}"
+}
 class APITest(unittest.TestCase):
 
   def test_category_get(self):
     image_base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='
-    res = requests.post(ENDPOINT+'categories', json={'description':'test description', 'image':image_base64})
-    logging.debug(res.json())
+    res = requests.post(
+      ENDPOINT+'categories/', 
+      json={'description':'test description', 'image':image_base64},
+      headers=headers
+      )
     self.assertEqual(res.status_code, 200)
+    self.assertIsInstance(res.json(),list)
+    self.assertIsInstance(res.json()[0],list)
+    self.assertIsInstance(res.json()[0][0],str)
+    logging.info(res.json())
 
   def test_category_get_text_only(self):
-    res = requests.post(ENDPOINT+'categories', json={'description':'test description'})
-    logging.debug(res.json())
+    res = requests.post(
+      ENDPOINT+'categories/', 
+      json={'description':'test description'},
+      headers=headers
+      )
     self.assertEqual(res.status_code, 200)
+    self.assertIsInstance(res.json(),list)
+    self.assertIsInstance(res.json()[0],list)
+    self.assertIsInstance(res.json()[0][0],str)
+    logging.info(res.json())
 
 if __name__ == '__main__':
   unittest.main()
