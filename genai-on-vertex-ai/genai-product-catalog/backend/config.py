327b5b1dfd164751693ee83ffd1ff01375c0fcad
Vijay Reddy <reddyv@google.com>
2023-11-16 19:20:21 +0000

    switch to flipkart

--- genai-on-vertex-ai/genai-product-catalog/backend/config.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/config.py
@@ -7,24 +7,29 @@ PROJECT = 'solutions-2023-mar-107'
 LOCATION = 'us-central1'
 
 # Vertex Vector Search
-ENDPOINT_ID = '8767655253467201536'
-DEPLOYED_INDEX = 'muiltimodal_13K_train'
+ENDPOINT_ID = '5145507709805658112'
+DEPLOYED_INDEX = 'flipkart_muiltimodal_18K'
 NUM_NEIGHBORS = 7
 
 # BigQuery
-PRODUCT_REFERENCE_TABLE = 'solutions-2023-mar-107.mercari.13K_synthetic_attributes_embeddings'
-COLUMN_ID = 'id'
+PRODUCT_REFERENCE_TABLE = 'solutions-2023-mar-107.flipkart.18K_no_duplicate_with_attributes'
+COLUMN_ID = 'uniq_id'
 COLUMN_CATEGORIES = [ # List of category column names from root to leaf
     'c0_name',
     'c1_name',
     'c2_name',
+    'c3_name'
 ]
+COLUMN_ATTRIBUTES = 'attributes'
 ALLOW_TRAILING_NULLS = True # whether to allow trailing category levels to be 
                             # unspecified e.g (only top-level category is specified)
 
 # Category
 CATEGORY_DEPTH = len(COLUMN_CATEGORIES) # number of levels in category hierarchy to consider
 
+# Attributes
+ATTRIBUTES_FORMAT =  'key_value' # can be 'flat' or 'key_value'
+
 # Testing
 TEST_GCS_IMAGE = 'gs://genai-product-catalog/toy_images/shorts.jpg' # Any image you have access to in GCS
-TEST_PRODUCT_ID = 'm71252118759' # Any valid product ID in reference table
+TEST_PRODUCT_ID = '8f87b1af1e8ab42c1d559f2f9caf70bb' # Any valid product ID in reference table
