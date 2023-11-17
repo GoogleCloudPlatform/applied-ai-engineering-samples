a433270ebfbbf076e63c27a0bbd073c095e9d75b
Vijay Reddy <reddyv@google.com>
2023-11-17 18:15:09 +0000

    attribute generation

--- genai-on-vertex-ai/genai-product-catalog/backend/api.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/api.py
@@ -4,20 +4,73 @@ from typing import Optional
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
+import attributes
 import category
 import marketing
 
 class Product(BaseModel):
     description: str
+    category: Optional[list[str]] = None
     main_image_base64: Optional[str] = None
 
 app = FastAPI()
 
 @app.post("/v1/categories/")
-def suggest_categories(product: Product):
+def suggest_categories(product: Product) -> list[list[str]]:
+    """Suggest categories for product.
+    
+    Args:
+    - description: Sparse description of product
+    - category (optional): If one or more category levels is known
+        include this to restrict the suggestions space. NOT YET IMPLEMENTED
+        - example 1: ['Mens']
+            will only return suggestiongs with top level category 'Mens'
+        - example 2: ['Mens', 'Pants']
+            will only return suggestions with top level category 'Mens'
+            and second level category 'Pants'
+    - main_image_base64 (optional): base64 encoded string representing product
+            image.
+
+    Returns:
+
+    The category suggestions ordered from high to low confidence. Returned as
+    a list of lists. Each inner list represents one fully qualified category
+    with each string in the list representing a category level e.g. 
+    ['Mens', 'Pants', 'Jeans']
+    """
     return category.retrieve_and_rank(
         product.description, product.main_image_base64, base64=True)
 
-@app.get("/v1/marketing/")
-def generate_marketing_copy(description: str, attributes: list[str] = Query(None)):
+@app.post("/v1/marketing/")
+def generate_marketing_copy(
+    description: str, attributes: dict[str, str]) -> str:
+    """Generate Marketing Copy.
+    
+    Args:
+    - description: sparse description of product
+    - attributes: pass as JSON key value pairs e.g. {'color':'green', 'pattern': 'striped'}
+
+    Returns:
+    
+    Marketing copy that can be used for a product page.
+    """
     return marketing.generate_marketing_copy(description, attributes)
+
+@app.post("/v1/attributes/")
+def suggest_attributes(product: Product) -> dict[str,str]:
+    """Suggests attributes for product.
+
+    Args:
+    - description: Sparse description of product
+    - category (optional): If category is known this will be considered in
+        generating relevant attributes. NOT YET IMPLEMENTED
+    - main_image_base64 (optional): base64 encoded string representing product
+            image.
+
+    Returns:
+    
+    JSON dictionary representing attributes as key value pairs e.g. 
+    {'color':'green', 'pattern': 'striped'}
+    """
+    return attributes.generate_attributes(
+        product.description, product.category, product.main_image_base64, base64=True)
--- genai-on-vertex-ai/genai-product-catalog/backend/api_test.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/api_test.py
@@ -12,7 +12,7 @@ from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 
 ENDPOINT = 'http://localhost:8080/v1/'
-ENDPOINT = 'https://vijay-sandbox-335018.uc.r.appspot.com/v1/'
+#ENDPOINT = 'https://vijay-sandbox-335018.uc.r.appspot.com/v1/'
 
 open_id_connect_token = id_token.fetch_id_token(Request(), 'client_id')
 headers = {
@@ -20,7 +20,7 @@ headers = {
 }
 class APITest(unittest.TestCase):
 
-  def test_category_get(self):
+  def test_category(self):
     image_base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='
     res = requests.post(
       ENDPOINT+'categories/', 
@@ -33,7 +33,7 @@ class APITest(unittest.TestCase):
     self.assertIsInstance(res.json()[0][0],str)
     logging.info(res.json())
 
-  def test_category_get_text_only(self):
+  def test_category_text_only(self):
     res = requests.post(
       ENDPOINT+'categories/', 
       json={'description':'test description'},
@@ -46,16 +46,27 @@ class APITest(unittest.TestCase):
     logging.info(res.json())
 
   def test_generate_marketing_copy(self):
-    res = requests.get(
+    res = requests.post(
       ENDPOINT+'marketing/', 
-      params={
-        'description':'Kids jacket', 
-        'attributes': ['rainbow', 'reversable']},
+      params={'description':'Mens Hooded Puffer Jacket'},
+      json={'attributes': '{"color":"green", "material": "down"}'},
       headers=headers
       )
     self.assertEqual(res.status_code, 200)
     self.assertIsInstance(res.text,str)
     logging.info(res.text)
+
+  def test_attributes(self):
+    image_base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='
+    res = requests.post(
+      ENDPOINT+'attributes/', 
+      json={'description':'test description', 'image':image_base64},
+      headers=headers
+      )
+    self.assertEqual(res.status_code, 200)
+    self.assertIsInstance(res.json(),dict)
+    self.assertGreater(len(res.json()),0)
+    logging.info(res.json())
 
 if __name__ == '__main__':
   unittest.main()
