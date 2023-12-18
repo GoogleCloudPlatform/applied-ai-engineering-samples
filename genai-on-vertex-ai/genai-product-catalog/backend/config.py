35d36210363d1e884c99ff152b4107f058b4acf4
Vijay Reddy <reddyv@google.com>
2023-12-18 20:34:41 +0000

    templatize

--- genai-on-vertex-ai/genai-product-catalog/backend/config.py
+++ genai-on-vertex-ai/genai-product-catalog/backend/config.py
@@ -17,17 +17,23 @@
 Update as needed to match your enivornment
 """
 # GCP
-PROJECT = 'solutions-2023-mar-107'
-LOCATION = 'us-central1'
+PROJECT = '<YOUR GCP PROJECT ID>'
+LOCATION = '<YOUR GCP PROJECT LOCATION>' # e.g. 'us-central1'
 
 # Vertex Vector Search
-ENDPOINT_ID = '5145507709805658112'
-DEPLOYED_INDEX = 'flipkart_muiltimodal_18K'
+ENDPOINT_ID = '<YOUR VERTEX VECTOR SEARCH ENDPOINT ID>' # e.g. '1641918305943945216'
+DEPLOYED_INDEX = '<YOUR VERTEX VECTOR SEARCH DEPLOYED INDEX ID>' # e.g. 'flipkart_1702030773989'
 NUM_NEIGHBORS = 7
+FILTER_CATEGORIES = [ # List of category filter names from root to leaf
+    'L0',
+    'L1',
+    'L2',
+    'L3'
+]
 
 # BigQuery
-PRODUCT_REFERENCE_TABLE = 'solutions-2023-mar-107.flipkart.18K_no_duplicate_with_attributes'
-COLUMN_ID = 'uniq_id'
+PRODUCT_REFERENCE_TABLE = '<YOUR BQ TABLE NAME>' # e.g. 'project_name.flipkart.products'
+COLUMN_ID = 'id'
 COLUMN_CATEGORIES = [ # List of category column names from root to leaf
     'c0_name',
     'c1_name',
@@ -42,6 +48,7 @@ ALLOW_TRAILING_NULLS = True # whether to allow trailing category levels to be
 # Category
 CATEGORY_DEPTH = len(COLUMN_CATEGORIES) # number of levels in category hierarchy to consider
 
-# Testing
+# Testing - Update these for unit tests to run properly
 TEST_GCS_IMAGE = 'gs://genai-product-catalog/toy_images/shorts.jpg' # Any image you have access to in GCS
 TEST_PRODUCT_ID = '8f87b1af1e8ab42c1d559f2f9caf70bb' # Any valid product ID in reference table
+TEST_CATEGORY_L0 = 'Clothing' # Any valid top level category. Case sensitive
