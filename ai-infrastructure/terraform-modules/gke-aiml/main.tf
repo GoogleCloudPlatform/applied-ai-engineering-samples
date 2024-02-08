82d58b65a9c4eefa95e243593a77cdd5bf44ead8
jarokaz <jarekk@google.com>
2024-02-08 13:55:42 +0000

    Added versions.tf to modules

--- ai-infrastructure/terraform-modules/gke-aiml/main.tf
+++ ai-infrastructure/terraform-modules/gke-aiml/main.tf
@@ -12,19 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-terraform {
-  required_version = ">=1.5.1"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.6.0, < 6.0.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 5.6.0, < 6.0.0"
-    }
-  }
-}
 
 data "google_project" "project" {
   project_id = var.project_id
