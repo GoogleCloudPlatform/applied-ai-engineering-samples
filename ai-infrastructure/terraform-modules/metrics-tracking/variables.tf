4db93e99da381205c69907428b97362f115f37bf
jarokaz <jarekk@google.com>
2024-02-19 17:39:52 +0000

    Updated README files in Terraform modules

--- ai-infrastructure/terraform-modules/metrics-tracking/variables.tf
+++ ai-infrastructure/terraform-modules/metrics-tracking/variables.tf
@@ -12,17 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "automation" {
-  description = "Automation configs"
-  type = object({
-    outputs_bucket = string
-  })
-  default = {
-    outputs_bucket = null
-  }
-  nullable = false
-}
-
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
